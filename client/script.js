e92dcf8c2f22da94329fda06bc362d3d6d7af2f8
Oshioke-Salaki <salaki1902@gmail.com>
2023-01-05 19:57:44 -0600

    Created the server side of the application and consumed the backend

--- client/script.js
+++ client/script.js
@@ -80,6 +80,35 @@ const handleSubmit = async(e) => {
 
     loader(messageDiv);
 
+    //fetch data from the server -> bot's response
+    const response = await fetch('http://localhost:5000', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt'),
+        }),
+    });
+
+    clearInterval(loadInterval);
+    messageDiv.innerHTML = '';
+
+    if (response.ok) {
+        const data = await response.json();
+        const parsedData = data.bot.trim();
+
+        console.log(parsedData);
+
+        typetext(messageDiv, parsedData);
+    } else {
+        const err = await response.text();
+
+        messageDiv.innerHTML = 'Something went wrong';
+
+        alert(err);
+    }
+
     e.preventDefault();
 };
 
